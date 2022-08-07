0b28eb991add4318df9c1d53832d59d5664c8e40
guardofparadise <excelent91@mail.ru>
2022-08-07 20:55:44 +0300

    updated code

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,36 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+	function menuOnClick() {
+		document.getElementById("menu-bar").classList.toggle("change");
+		document.getElementById("nav").classList.toggle("change");
+		document.getElementById("menu-bg").classList.toggle("change-bg");
+	}
+
+	document.getElementById("menu-bar").addEventListener('click', menuOnClick)
+
+	// Change option selected
+	const label = document.querySelector('.dropdown__filter-selected')
+	const options = Array.from(document.querySelectorAll('.dropdown__select-option'))
+
+	options.forEach((option) => {
+		option.addEventListener('click', () => {
+			label.textContent = option.textContent
+		})
+	})
+
+	// Close dropdown onclick outside
+	document.addEventListener('click', (e) => {
+		const toggle = document.querySelector('.dropdown__switch')
+		const element = e.target
+
+		if (element == toggle) return;
+
+		const isDropdownChild = element.closest('.dropdown__filter')		
+		
+		if (!isDropdownChild) {
+			toggle.checked = false
+		}
+	})
 
 })
